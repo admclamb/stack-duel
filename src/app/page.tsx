df33ea9e4eb660fb96fd01d7cf6fd36159e3169d
admclamb <adylanmclamb@yahoo.com>
2026-09-19 14:57:00 -0400

    Update stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-        Hello World 4
+        Hello World 10
       </h1>
     </main>
   );
