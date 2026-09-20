d563e743e9d47ceafe427cc3e5435401942d0f06
admclamb <adylanmclamb@yahoo.com>
2026-09-20 00:38:28 -0400

    Fix the e2e test

--- e2e/smoke.spec.ts
+++ e2e/smoke.spec.ts
@@ -1,8 +1,9 @@
 import { expect, test } from "@playwright/test";
 
-test("home page loads", async ({ page }) => {
+test("landing page loads", async ({ page }) => {
   await page.goto("/");
   await expect(
-    page.getByRole("heading", { name: /hello world/i }),
+    page.getByRole("heading", { name: /stop coding alone/i }),
   ).toBeVisible();
+  await expect(page.getByTestId("sign-up-button")).toBeVisible();
 });
